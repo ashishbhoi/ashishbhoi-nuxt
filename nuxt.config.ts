94f4276311f289c0c784e8b030c188f6b103fe83
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2023-02-25 16:23:43 +0530

    turnstile added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/turnstile'],
+
+  turnstile: {
+    siteKey: '0x4AAAAAAACwG4kjB4kyNBmu',
+  },
+
+  runtimeConfig: {
+    turnstile: {
+      secretKey: process.env.NUXT_TURNSTILE_SECRET_KEY || '',
+    },
+  },
 
 })
