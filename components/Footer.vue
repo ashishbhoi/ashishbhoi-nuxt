cc9b6100ef3ec94411c2bc9c577f1369f2c8f902
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2023-02-26 11:26:06 +0530

    Footer added

---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <hr />
+  <footer class="flex items-center justify-center py-4">
+    <span class="text-sm text-gray-500 text-center dark:text-gray-400">© 2023 <a href="https://www.ashishbhoi.com/" class="hover:underline">Ashish Kumar Bhoi </a>. All Rights Reserved.
+    </span>
+  </footer>
+
+</template>
