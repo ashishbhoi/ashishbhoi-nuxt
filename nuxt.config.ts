d8fa16249a6f04995e1a9396a4d56c31f9f4cab8
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2023-05-02 15:40:07 +0530

    Default html lang and head added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    // @ts-ignore "DON'T KNOW HOW TO FIX"
+    app:{
+      head: {
+          charset: 'utf-8',
+          htmlAttrs: {
+              lang: 'en-US'
+          },
+          meta: [
+              {name: "mobile-web-app-capable", content: "yes"}
+          ]
+      }
+    },
     modules: [
         '@nuxtjs/turnstile',
         '@nuxtjs/tailwindcss'
